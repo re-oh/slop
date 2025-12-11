4a96d7bcb681a81f9e101a7161b48a91cf434a91
Lukasz Dabrowski <dabrowskilucas2@gmail.com>
2025-12-11 13:57:48 +0100

    expanded banned os functionality

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
-fn main() {
-    let os = std::env::consts::OS;
+use std::process::exit;
 
-    match os {
-        "windows" => println!("pussy ass motherfucker get linux then you can use slop"),
-        "macos" => println!("labubu matcha faggot invest some of that 2000$ macbook into a 500$ laptop with linux pussy"),
-        _ => ()
+fn check_os() {
+    match std::env::consts::OS {
+        "widnows" => {
+            println!("pussy ass motherfucker get linux then you can use slop");
+            exit(69);
+        }
+        "macos" => {
+            println!("you labubu matcha faggot, invest some of that 2000$ macbook into a 500$ laptop with linux, pussy");
+            exit(69);
+        }
+        _ => (),
     }
+}
+
+fn main() {
+    // Check os only non-windows and non-macos systems are allowed.
+    check_os();
+    let stdout = std::io::stdout();
 }
