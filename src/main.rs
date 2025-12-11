df8f22fcc062d31bdac75f0564e07f53d32409b5
Lukasz Dabrowski <dabrowskilucas2@gmail.com>
2025-12-11 13:36:47 +0100

    cipka

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let os = std::env::consts::OS;
+
+    match os {
+        "windows" => println!("pussy ass motherfucker get linux then you can use slop"),
+        "macos" => println!("labubu matcha faggot invest some of that 2000$ macbook into a 500$ laptop with linux pussy"),
+        _ => ()
+    }
+}
